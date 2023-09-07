0dc8587d41389d3b15a1dfb74d729422af148d1d
sue-lima <suelen.ferreira94@gmail.com>
2023-09-06 22:12:05 -0300

    conversion working

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive, computed } from 'vue'
 import api from '@/services/api'
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
 
 export default {
   setup() {
     const symbol = ref([])
+    const state = reactive({
+      selectedOptionFrom: '',
+      selectedOptionTo: '',
+      amountValue: ''
+    })
+
+    const rules = computed(() => {
+      return {
+        selectedOptionFrom: { required },
+        selectedOptionTo: { required },
+        amountValue: { required },
+      }
+    })
+
+    const v$ = useVuelidate(rules, state)
 
     const fetchSymbol = () =>
       api.get('/symbols').then((response) => {
@@ -18,20 +35,40 @@ export default {
 
     onMounted(fetchSymbol)
 
-    return { symbol }
+    return { symbol, state, v$ }
   },
 
   data() {
     return {
-      selectedOptionFrom: '',
-      selectedOptionTo: ''
-    }
+      customInputMessage: 'This field is required.',
+      customSelectMessage: 'Please choose the currency.',
+    };
   },
+
   methods: {
     exibirValorSelecionado() {
-      console.log('Valor selecionado From:', this.selectedOptionFrom)
-      console.log('Valor selecionado To:', this.selectedOptionTo)
-    }
+      api.get(`/latest?/source=ecb&base=${this.state.selectedOptionFrom}`)
+        .then((response) => {
+          const amount = this.state.amountValue;
+          const currencyTo = this.state.selectedOptionTo;
+          const currencyFrom = this.state.selectedOptionFrom;
+          const rate = response.data.rates[currencyTo];
+          this.v$.$validate()
+          function convert(amount, rate) {
+            return amount * rate;
+          }
+          if (!this.v$.$error) {
+            const result = convert(amount, rate);
+            document.querySelector(".display-result").innerHTML = `${amount} ${currencyFrom.toUpperCase()} equal to ${currencyTo} ${result.toFixed(2)}`;
+          } else {
+            alert('Select all fields correctly');
+          }
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+      return false;
+    },
   }
 }
 </script>
@@ -44,21 +81,26 @@ export default {
   </header>
   <main class="grow flex justify-center items-center">
     <div class="rounded-xl p-5 gap-2 flex border text-center">
-      <input type="number" id="amount" step="0.01" />
-      <select v-model="selectedOptionFrom" id="currency-from">
+      <input v-model="state.amountValue" type="number" id="amount" step="0.01" placeholder="Enter the amount"/>
+      <span v-if="v$.amountValue.$error">{{ customInputMessage }}</span>
+      <select v-model="state.selectedOptionFrom" id="currency-from" required>
         <option value="" disabled selected hidden>Please Choose</option>
-        <option v-for="(teste, i) in symbol" :key="i" :value="teste.code">
-          {{ teste.code }} - {{ teste.description }}
+        <option v-for="(currency, i) in symbol" :key="i" :value="currency.code">
+          {{ currency.code }} - {{ currency.description }}
         </option>
       </select>
+      <span v-if="v$.selectedOptionFrom.$error">{{ customSelectMessage }}</span>
       <p>--</p>
-      <select v-model="selectedOptionTo" id="currency-to">
+      <select v-model="state.selectedOptionTo" id="currency-to" required>
         <option value="" disabled selected hidden>Please Choose</option>
-        <option v-for="(teste, i) in symbol" :key="i" :value="teste.code">
-          {{ teste.code }} - {{ teste.description }}
+        <option v-for="(currency, i) in symbol" :key="i" :value="currency.code">
+          {{ currency.code }} - {{ currency.description }}
         </option>
       </select>
+      <span v-if="v$.selectedOptionTo.$error">{{ customSelectMessage }}</span>
       <button @click="exibirValorSelecionado" class="bg-gray-500 p-3">Converter</button>
+    </div>
+    <div class="display-result d-flex justify-content-center text-success">
     </div>
   </main>
   <footer class="grow-0">
