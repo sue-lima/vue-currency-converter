602487de0aff7e29d26c21259cacd0de28d6a42b
sue-lima <suelen.ferreira94@gmail.com>
2023-09-19 21:08:13 -0300

    chart update

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <Nu v-if="loaded" :data="chartData" :options="chartOptions" />
+  <div class="flex flex-col pt-7">
+    <div class="flex justify-between">
+      <button @click="exibirValor(3)" class="bg-pink-500 w-11 h-7 rounded-2xl">3D</button>
+      <button @click="exibirValor(7)" class="bg-pink-500 w-11 h-7 rounded-2xl">7D</button>
+      <button @click="exibirValor(15)" class="bg-pink-500 w-11 h-7 rounded-2xl">15D</button>
+      <button @click="exibirValor(30)" class="bg-pink-500 w-11 h-7 rounded-2xl">1M</button>
+      <button @click="exibirValor(365)" class="bg-pink-500 w-11 h-7 rounded-2xl">1Y</button>
+      <button @click="exibirValor(730)" class="bg-pink-500 w-11 h-7 rounded-2xl">2Y</button>
+      <button @click="exibirValor(1826)" class="bg-pink-500 w-11 h-7 rounded-2xl">5Y</button>
+      <button @click="exibirValor(3652)" class="bg-pink-500 w-11 h-7 rounded-2xl">10Y</button>
+    </div>
+    <Nu v-if="loaded" :data="chartData" :options="chartOptions"/>
   </div>
 </template>
 
@@ -26,20 +36,21 @@ export default {
     selectedOptionFrom: String,
     selectedOptionTo: String,
   },
-  
+
   data() {
     return {
       loaded: false,
       chartData: {
         labels: [],
         datasets: [{
-          label: '', 
-          data: [] 
+          label: '',
+          data: []
         }]
       },
       chartOptions: {
         responsive: true,
       },
+      selectedValue: 3,
     };
   },
 
@@ -76,10 +87,14 @@ export default {
       const url = `/timeseries?start_date=${startDate}&end_date=${endDate}&base=${this.selectedOptionFrom}`;
       return api.get(url);
     },
+    exibirValor(valorClicado) {
+      this.loaded = false;
+      this.selectedValue = valorClicado;
+      this.loadData();
+    },
     calculateStartDate() {
-      const daysToSubtract = 3;
       const date = new Date();
-      date.setDate(date.getDate() - daysToSubtract);
+      date.setDate(date.getDate() - this.selectedValue);
       return this.formatDate(date);
     },
     formatDate(date) {
